d5b1a33e82ef187c496228fd8b2f7a9ab2b35616
MarcoColloca <marco.colloca3511@gmail.com>
2024-10-06 20:36:50 +0200

    Fix → sistemata homepage in mobile

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -14,18 +14,16 @@
     </div>
 
     <section class="my-5 hero container">
-        <div class="h-100 ">
-            <div class="hero-container text-center">
-                <p class="text-xl w-1/2">
-                    Benvenuti. Questo sito è stato creato per raggruppare la Lore della mia ambientazione di d&d 5.5e.
-                    Naviga pure tra le pagine per scoprire quello che ti serve. Nella Navbar in alto, trovi i vari argomenti.
-                    In ogni pagina sarà invece presente un indice per mostrare le diverse informazioni suddivise per macroaree.                    
-                </p>
-                <p class="text-red-500 bg-black px-3 py-1 w-3/12 text-lg">
-                    Tenete sempre in considerazione che è un work in progress, al momento molto in progress. 
-                    Verrà aggiornato in tempo reale nel corso della campagna.
-                </p>
-            </div>
+        <div class="hero-container text-center">
+            <p class="text-xl w-12/12 lg:w-1/2">
+                Benvenuti. Questo sito è stato creato per raggruppare la Lore della mia ambientazione di d&d 5.5e.
+                Naviga pure tra le pagine per scoprire quello che ti serve. Nella Navbar in alto, trovi i vari argomenti.
+                In ogni pagina sarà invece presente un indice per mostrare le diverse informazioni suddivise per macroaree.                    
+            </p>
+            <p class="text-red-500 bg-black px-3 py-1 w-12/12 lg:w-3/12 text-lg">
+                Tenete sempre in considerazione che è un work in progress, al momento molto in progress. 
+                Verrà aggiornato in tempo reale nel corso della campagna.
+            </p>
         </div>
     </section>
 </template>
@@ -34,14 +32,14 @@
 
 <style lang="scss" scoped>
 .hero {
-    height: 600px;
+    min-height: 600px;
     background-color: antiquewhite;
     border-radius: 18px;
     color: black;
 }
 
 .hero-container {
-    height: 100%;
+    min-height: 600px;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
