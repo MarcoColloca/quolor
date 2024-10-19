b237a94cc3f8d0ff9c03130d58e812294d151427
MarcoColloca <marco.colloca3511@gmail.com>
2024-10-19 20:40:39 +0200

    Refactor → migliorato il submenu, quando presente

--- src/components/single-components/Header/Navbar.vue
+++ src/components/single-components/Header/Navbar.vue
@@ -16,22 +16,21 @@ const toggleSubMenu = headerStore.toggleSubMenu
     <nav class="justify-content-center align-items-center navbar navbar-expand-lg">
         <ul class="flex gap-5 text-white">
             <li v-for="(link, index) in navbarLinks" :key="index" class="relative">
-                <RouterLink  :to="{name: link.pathName}" class="nav-link">{{ link.name }}</RouterLink>
-                
-                <!-- Verifica la presenza dei subPaths per mostrare il menu a tendina -->
-                <div v-if="link.subPaths" @click="toggleSubMenu(link)" class="dropdown-toggle cursor-pointer font-bold">
-                    Lista
-                </div>
-                
-                <!-- Submenu, visibile solo se showSubMenu è true -->
-                <transition name="slide-fade">
-                    <ul v-if="link.showSubMenu" class="submenu absolute bg-white text-black rounded-md shadow-lg mt-1">
-                        <li v-for="(subLink, index) in link.subPaths" :key="index"
-                        >
-                            <RouterLink @click="toggleSubMenu(link)" :to="{name: subLink.pathName}" class="block">{{ subLink.name }}</RouterLink>                            
-                        </li>
-                    </ul>
-                </transition>
+              
+              <!-- Verifica la presenza dei subPaths per mostrare il menu a tendina -->
+              <div v-if="link.subPaths" @click="toggleSubMenu(link)" class="nav-link cursor-pointer font-bold">
+                {{ link.name }}
+              </div>
+              <RouterLink v-else :to="{name: link.pathName}" class="nav-link">{{ link.name }}</RouterLink>
+              
+              <!-- Submenu, visibile solo se showSubMenu è true -->
+              <transition name="slide-fade">
+                  <ul v-if="link.showSubMenu" class="submenu absolute bg-white text-black rounded-md shadow-lg mt-1">
+                    <li v-for="(subLink, index) in link.subPaths" :key="index">
+                      <RouterLink @click="toggleSubMenu(link)" :to="{name: subLink.pathName}" class="block">{{ subLink.name }}</RouterLink>                            
+                    </li>
+                  </ul>
+              </transition>
             </li>            
         </ul>  
     </nav>
@@ -56,25 +55,22 @@ li {
     }
   }
 
-  .dropdown-toggle {
-    margin-left: 5px;
-    font-size: 0.8rem;
-  }
-
   .submenu {
     min-width: 150px;
     z-index: 100;
     border: 1px solid coral;
     background-color: white;
     border-radius: 8px;
+    left: 50%; /* Modifica */
+    transform: translateX(-50%); /* Modifica */
     li {
-        a{
-            padding: 8px 16px;
-        }
-        &:hover{
-            background-color: coral;
-            color: white;
-        }
+      a{
+        padding: 8px 16px;
+      }
+      &:hover{
+        background-color: coral;
+        color: white;
+      }
     }
   }
 }
